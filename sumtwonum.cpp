79ddecb41f126953ce5196b3f156c023a78bbd48
ashiknr1010 <ashikrenjit2004@gmail.com>
2025-09-06 23:19:26 +0530

    Create sumtwonum.cpp

---
+++ sumtwonum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a,b,sum;
+    
+    cout<<"enter the 1st value";
+    cin>>a;
+    
+    cout<<"enter the 2nd value";
+    cin>>b;
+    
+    sum=a+b;
+    cout<<"sum="<<sum;
+
+}
